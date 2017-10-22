2edd5e2c379746af37d5d16bcb5db7cbaa7aa271
Luke Plaster <me@lukep.org>
2017-10-22 20:09:46 +0800

    Showing the exception info in the new StringBuilder

--- neo-gui/UI/InvokeContractDialog.cs
+++ neo-gui/UI/InvokeContractDialog.cs
@@ -288,34 +288,14 @@ namespace Neo.UI
                     }
                 }
             } else if (ExecutionEngine.LastException != null) {
-                StringBuilder sb = new StringBuilder();
-                  try {
-                     foreach (StackItem item in ExecutionEngine.LastEvaluationStack) {
-                        sb.AppendLine(BitConverter.ToString(item.GetByteArray()));
-                     }
-                  } catch {
-                     foreach (StackItem item in ExecutionEngine.LastEvaluationStack) {
-                        sb.Append("Array[");
-                        foreach (StackItem item2 in item.GetArray()) {
-                           sb.Append(BitConverter.ToString(item2.GetByteArray()));
-                           sb.Append(", ");
-                        }
-                        sb.AppendLine("]");
-                     }
-                  }
-
                 string message = String.Format("{0} {1} {2} {3}\nEvaluation stack:\n{4}",
                       Strings.ExecutionFailed,
-                      ExecutionEngine.LastException.Message,
-                      ExecutionEngine.LastException.StackTrace,
                       sb.ToString());
-
                 MessageBox.Show(message);
             } else {
                 MessageBox.Show(Strings.ExecutionFailed + ". No exception message.");
             }
         }
-
 
         /**
          * clear loaded contract details
